3865307f5ce8c56534dfb30375c3f6f5476478b7
F3L1P36 <felipe2guzzb@gmail.com>
2023-11-11 23:44:59 -0600

    creacion de carpeta pages y configuracion de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-
-
-import React from 'react'
+import{ Routes, Route, BrowserRouter} from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
+import Signin from './pages/Signin';
+import Profile from './pages/Profile';
+import SigUp from './pages/SigUp';
 
 export default function App() {
   return (
-    <div className=''>Helloo world</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/sign-in' element={<Signin/>}/>
+      <Route path='/sign-up' element={<SigUp/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+
+      
+    </Routes>
+    </BrowserRouter>
   )
 }
---
+++ src/pages/SigUp.jsx
@@ -0,0 +1,8 @@
+    import React from 'react'
+    
+    export default function SigUp() {
+      return (
+        <div>SigOut</div>
+      )
+    }
+    
